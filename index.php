028913dc5278848d96c08d6e4ea9321c5bec877e
Corfaya <lauracorrado0696@gmail.com>
2024-09-04 15:10:46 +0200

    [UPD] form added in index.php

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
     <title>Badwords PHP</title>
 </head>
 <body>
-    
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <form action="" method="GET">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-floating">
+                                <label for="paragaph">Scrivere qualcosa qui</label>
+                                <textarea class="form-control" name="paragrafo"  id="paragaph"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-6 my-3">
+                            <div class="form-group">
+                                <label for="censored">Da censurare:</label>
+                                <input type="text" class="form-control" name="bad_word" placeholder="Parola da censurare" id="censored">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-danger text-white fw-bolder">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
