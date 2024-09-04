ba63a48629e3b569c745f179f853e0ed99bc6ce2
Corfaya <lauracorrado0696@gmail.com>
2024-09-04 15:50:39 +0200

    [UPD] Exercise done

--- badwords.php
+++ badwords.php
@@ -1,9 +1,10 @@
 <?php
     $paragraph = $_GET["paragrafo"];
     $bad_word = $_GET["bad_word"];
+    $length = strlen($paragraph);
     $new_paragraph = str_replace($bad_word, "***", $paragraph);
-    $length = strlen($new_paragraph);
-    var_dump($paragraph, $length);
+    $length_censored = strlen($new_paragraph);
+    // var_dump($paragraph, $length);
 ?>
 
 <!DOCTYPE html>
@@ -23,11 +24,20 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <h3>Il tuo testo:</h3>
-                <em class="mt-2"><?php echo $new_paragraph ?></em>
+                <h3>Il tuo testo senza censura:</h3>
+                <em class="mt-2"><?php echo $paragraph ?></em>
             </div>
             <div class="col-12">
                 <h3 class="fs-4 mt-4">La lunghezza del tuo testo è pari a: <span class="fs-5 fw-bold d-inline text-danger"><?php echo $length ?></span></h3>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Il tuo testo con censura:</h3>
+                <em class="mt-2"><?php echo $new_paragraph ?></em>
+            </div>
+            <div class="col-12">
+                <h3 class="fs-4 mt-4">La lunghezza del tuo testo con censura è pari a: <span class="fs-5 fw-bold d-inline text-danger"><?php echo $length_censored ?></span></h3>
             </div>
         </div>
     </div>
